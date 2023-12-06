d55456ef3e0f91019da6d5cacf190db8c64918f6
Adam Pilak <pilak.adam@gmail.com>
2023-12-06 19:19:28 +0100

    search-list in progress

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,5 +1,39 @@
-import { baseApi } from "./base"
+import { baseApi } from './base'
+
+export type Weather = {
+  last_updated_epoch: number
+  last_updated: string
+  temp_c: number
+  temp_f: number
+  is_day: number
+  condition: Condition
+  wind_mph: number
+  wind_kph: number
+  wind_degree: number
+  wind_dir: string
+  pressure_mb: number
+  pressure_in: number
+  precip_mm: number
+  precip_in: number
+  humidity: number
+  cloud: number
+  feelslike_c: number
+  feelslike_f: number
+  vis_km: number
+  vis_miles: number
+  uv: number
+  gust_mph: number
+  gust_kph: number
+}
+
+type Condition = {
+  text: string
+  icon: string
+  code: number
+}
 
 export function getWeather(query: string) {
-	return baseApi.get(`/current.json?key=a0c075ba8c1d4078baf165547230512&q=${query}`).then(res => console.log(res.data))
+  return baseApi
+    .get(`/current.json?key=a0c075ba8c1d4078baf165547230512&q=${query}`)
+    .then((res) => res.data.current as Weather)
 }
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,15 +1,24 @@
-import { SearchItem } from "../components/Searchitem";
+import { useEffect, useState } from 'react'
+import { SearchList } from '../components/SearchList'
+import styles from './Search.module.css'
+import { getWeather } from '../api/weather'
 
 export function Search() {
-	return (
-		<div className="search-container">
-			<div className="search">
-				<input type="text" />
-				<button>Cancel</button>
-			</div>
-			<ul className="search-list">
-				<SearchItem></SearchItem>
-			</ul>
-		</div>
-	)
+  const [results, setResults] = useState([''])
+  const [recentlyViewed, setRecentlyViewed] = useState(['', ''])
+
+  useEffect(() => {
+	setResults(getWeather('london'))
+  }, [])
+
+  return (
+    <div className={styles['search-container']}>
+      <div className={styles['search']}>
+        <input type="text" />
+        <button>Cancel</button>
+      </div>
+      <SearchList header="Results" items={results}></SearchList>
+      <SearchList header="Recently viewed" items={recentlyViewed}></SearchList>
+    </div>
+  )
 }
